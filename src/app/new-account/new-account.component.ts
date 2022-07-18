069ebbb8bd64a52b1835c20e7ffa20aa3a4248a0
Hevesi Istvan <hevesiistvan71@hotmail.com>
2022-07-18 12:11:16 +0200

    107 Injecting the Logging Service into Components - kész

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,20 +1,26 @@
 // prettier-ignore
 import { Component, EventEmitter, Output } from '@angular/core';
+// prettier-ignore
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
   // prettier-ignore
   @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
 
-  // prettier-ignore
+  constructor(private loggingService: LoggingService) {}
   onCreateAccount(accountName: string, accountStatus: string) {
+    // prettier-ignore
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus,
     });
+    // prettier-ignore
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
